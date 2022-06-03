747dfb7a5779611c7730aa482b6f936870bf7879
AdrianSkar <adrian@adrianskar.com>
2022-06-03 15:25:52 +0200

    refactor[api]: 2nd try

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	let selectIDs = ['#input-base', '#input-target'];
 
-	selectIDs.forEach(value => {
-		$(`${value}`).addEventListener('change', evt => getData('convert', evt));
-	});
+	$('#convert-btn').addEventListener('click', evt => getData('convert', evt));
 });
 
 function getData(action, evt) {
@@ -21,43 +19,40 @@ function getData(action, evt) {
 	fetch(url)
 		.then(resp => resp.json())
 		.then(content => {
-			let main = $('main');
-			main.innerHTML += `<h2>Api key: ${content.key}</h2>`;
-			console.log(this);
-
 			if (action === 'convert') {
+				// User wants to convert
 				let [baseCur, targetCur, inputAmt, resultDiv] = [
 					$('#input-base'),
 					$('#input-target'),
 					$('#input-amt'),
 					$('#result'),
 				];
-				console.log(baseCur.value, targetCur.value);
-				// Get data from API to convert
+				// Get data from exchange API to convert
 				let exURL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${baseCur.value}&to_currency=${targetCur.value}&apikey=${content.key}`;
-				console.log(exURL);
+				// console.log(exURL);
 				fetch(exURL)
 					.then(response => response.json())
 					.then(response => {
-						console.log(response);
-						console.log(
-							response['Realtime Currency Exchange Rate']['5. Exchange Rate']
-						);
+						console.log(exURL);
+						// console.log(response);
+						// console.log(
+						// 	response['Realtime Currency Exchange Rate']['5. Exchange Rate']
+						// );
 						let rate =
 							response['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-						resultDiv.innerHTML += `Input amt: ${
-							inputAmt.value
-						}, rate: ${rate}, result: ${inputAmt.value * rate}`;
+						resultDiv.innerHTML = `Result: ${(inputAmt.value * rate).toFixed(
+							2
+						)} (rate: ${rate})`;
 					})
 					.catch(err => console.error(err));
 
-				console.log('target', evt.target);
-				console.log(action, evt);
+				// console.log('target', evt.target);
+				// console.log(action, evt);
 			} else {
+				// User wants to get data
 				let apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${action}&interval=5min&apikey=${content.key}`;
 				// Get data from API
 				fetch(apiUrl)
-					// `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${content.key}`
 					// `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${content.key}`
 					.then(response => response.json())
 					.then(response => console.log(response))
